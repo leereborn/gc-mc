fix loss=nan by adding leakyRelu and softmax activation to attn

gcmc/layers.py
@@ -243,9 +243,16 @@ class AttentionalStackGCN(Layer):
         with tf.variable_scope(self.name + '_vars'):
             self.vars['weights_u'] = weight_variable_random_uniform(input_dim, output_dim, name='weights_u')
             self.vars['attn_weights']=[]
-            self.vars['attn_weights'].append(tf.random.uniform(shape=[output_dim/num_support,1])) #try weight_variable_random_uniform?
-            self.vars['attn_weights'].append(tf.random.uniform(shape=[output_dim/num_support,1])) # Use output dim because since the features are transformed to the output_dim.
-            #self.vars['attn_coefs_v'] = tf.random.uniform(shape=[input_dim,1])
+            attn1 = tf.get_variable(name='attn_self',shape=(output_dim/num_support,1),initializer=tf.glorot_uniform_initializer,regularizer=tf.keras.regularizers.l2(l=0.01))
+            attn2 = tf.get_variable(name='attn_neigh',shape=(output_dim/num_support,1),initializer=tf.glorot_uniform_initializer,regularizer=tf.keras.regularizers.l2(l=0.01))
+            #print(attn1)
+            #import pdb; pdb.set_trace()
+            self.vars['attn_weights'].append(attn1) #try weight_variable_random_uniform?
+            self.vars['attn_weights'].append(attn2) # Use output dim because since the features are transformed to the output_dim.
+            #self.vars['attn_weights'].append(tf.Variable(tf.glorot_uniform_initializer()((output_dim/num_support,1))))
+            #self.vars['attn_weights'].append(tf.Variable(tf.glorot_uniform_initializer()((output_dim/num_support,1))))
+            #self.vars['attn_weights'].append(tf.Variable(tf.initializers.he_normal()((output_dim/num_support,1))))
+            #self.vars['attn_weights'].append(tf.Variable(tf.initializers.he_normal()((output_dim/num_support,1))))
             if not share_user_item_weights:
                 self.vars['weights_v'] = weight_variable_random_uniform(input_dim, output_dim, name='weights_v')
 
@@ -282,10 +289,10 @@ class AttentionalStackGCN(Layer):
         #print(x_v.shape)
         #import pdb;pdb.set_trace()
         if self.sparse_inputs:
-            x_u = dropout_sparse(x_u, 1 - self.dropout, self.u_features_nonzero)
+            x_u = dropout_sparse(x_u, 1 - self.dropout, self.u_features_nonzero) 
             x_v = dropout_sparse(x_v, 1 - self.dropout, self.v_features_nonzero)
         else:
-            x_u = tf.nn.dropout(x_u, 1 - self.dropout)
+            x_u = tf.nn.dropout(x_u, 1 - self.dropout) # Is this consistent with the paper? 
             x_v = tf.nn.dropout(x_v, 1 - self.dropout)
 
         supports_u = []
@@ -307,6 +314,10 @@ class AttentionalStackGCN(Layer):
             attn_coef_u = tf.gather(attn_coef_u,self.list_u)
             attn_coef_v = attn_for_self_v + tf.transpose(attn_for_neighs_v) #(1682, 943)
             attn_coef_v = tf.gather(attn_coef_v,self.list_v)
+
+            # Add non-linearty
+            attn_coef_u = tf.nn.leaky_relu(attn_coef_u)
+            attn_coef_v = tf.nn.leaky_relu(attn_coef_v)
             
             sparse_supp = tf.sparse.reorder(support)
             sparse_supp_t = tf.sparse.reorder(support_transpose)
@@ -317,6 +328,10 @@ class AttentionalStackGCN(Layer):
             attn_coef_u += mask_supp
             mask_supp_t = -10e9 * (1.0 - dense_supp_t)
             attn_coef_v += mask_supp_t
+
+            # Apply softmax to coefficients
+            attn_coef_u = tf.nn.softmax(attn_coef_u)
+            attn_coef_v = tf.nn.softmax(attn_coef_v)
 
             #print(attn_coef_u.shape)
             #print(attn_coef_v.shape)
@@ -333,7 +348,7 @@ class AttentionalStackGCN(Layer):
 
         return u_outputs, v_outputs
 
-    def __call__(self, inputs):
+    def __call__(self, inputs): # implementation of function call operator
         with tf.name_scope(self.name):
             if self.logging and not self.sparse_inputs:
                 tf.summary.histogram(self.name + '/inputs_u', inputs[0])