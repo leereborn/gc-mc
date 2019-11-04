e07631c55be7d807ee69b1d26ed662a19cb5a3f5
Rui <leereborn92929@gmail.com>
2019-11-03 22:12:00 -0500

    update run_expriments

--- gcmc/run_experiments.py
+++ gcmc/run_experiments.py
@@ -1,7 +1,10 @@
 import os
 
+input_rates = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
 dropout_rates = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
 
-for input_rate in dropout_rates:
+for input_rate in input_rates:
     for rate in dropout_rates:
+        with open('results.txt','a') as f:
+            f.write('Input dropout rate: {}, dropout rate: {}\n'.format(input_rate,rate)) 
         os.system("python train.py -d ml_100k --accum stack -ido {} -do {} -nleft -nb 2 -e 1000 -wf --testing".format(input_rate,rate))
